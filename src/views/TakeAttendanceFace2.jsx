0b9ebafbbcf95e067b5daa501755ae9d92e51b50
Tow Wang Chiam <chiamtowwang@moneymatch.co>
2022-10-18 15:56:21 +0800

    added notifications, updated some styles, prevented multiple notifications

--- src/views/TakeAttendanceFace2.jsx
+++ src/views/TakeAttendanceFace2.jsx
@@ -3,61 +3,93 @@ import { io } from "socket.io-client";
 import { useCompreFace } from "../effects/useCompreFace";
 import { CompreFace } from "@exadel/compreface-js-sdk";
 import { isEmptyArray } from "../helpers/array";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import "react-notifications/lib/notifications.css";
 
 const socket = io("ws://localhost:3000", {
   reconnectionDelayMax: 10000,
-  //   auth: {
-  //     token: "123",
-  //   },
-  //   query: {
-  //     "my-key": "my-value",
-  //   },
 });
 
-const AttendeesList = (props) =>{
-  const {dataSrc} = props || {};
-    return !isEmptyArray(dataSrc)?  (<>{dataSrc&& dataSrc?.map((element) =>  <div>{element}</div>)}</> ): <div>nothing to see here </div>
-}
+const AttendeesList = (props) => {
+  const { dataSrc } = props || {};
+  return !isEmptyArray(dataSrc) ? (
+    <>
+      <h1>Attendees List</h1>
+      {dataSrc &&
+        dataSrc?.map((element, index) => (
+          <div key={`${element}-${index}`}>{element}</div>
+        ))}
+    </>
+  ) : (
+    <div>nothing to see here </div>
+  );
+};
 
-
-const TakeAttendanceFace = () => {
+const TakeAttendanceFace = (props) => {
+  // need to get session id
+  const [sessionId, setSessionId] = useState(
+    "e58e7759-357f-4fa3-94fc-685611593ec6"
+  );
   const [attendees, setAttendees] = useState([]);
-  const [isTakingAttendance, setIsTakingAttendance] = useState(false);
   const videoRef = useRef(null);
   const canvas1 = useRef(null);
   let stopRef = useRef(0);
-  let receivedMediaStream = null;
-  // const server = "http://localhost";
-  // const port = 8000;
-  // const DETECTIONKEY = "79385fad-5a3b-4abb-8b06-ae650e639b95";
-  // const RECOGNITIONKEY = `10ca1151-f830-4ce0-ac53-9714c77c3241`;
-  const core = new CompreFace(process.env.REACT_APP_SERVER, process.env.REACT_APP_SERVER_PORT);
-  // const detection_service = core.initFaceDetectionService(DETECTIONKEY);
-  const detection_service = core.initFaceDetectionService(process.env.REACT_APP_DETECTIONKEY);
-  const recognitionService = core.initFaceRecognitionService(process.env.REACT_APP_RECOGNITIONKEY);
+
+  const core = new CompreFace(
+    process.env.REACT_APP_SERVER,
+    process.env.REACT_APP_SERVER_PORT
+  );
+  const recognitionService = core.initFaceRecognitionService(
+    process.env.REACT_APP_COMPPREFACE_RECOGNITION_KEY
+  );
 
   useEffect(() => {
-    if (isTakingAttendance === true && videoRef && canvas1) {
-      setTimeout(fetchLoop, 3000);
-    }
-  }, [isTakingAttendance, videoRef, canvas1]);
+    socket.emit("findAllAttendance", {
+      sessionId: sessionId,
+    });
+
+    socket.on("findAllAttendance", (data) => {
+      setAttendees(data);
+    });
+    socket.on("newAttendance", (data) => {
+      setAttendees((prev) => {
+        const prevLength = prev?.length;
+        if (prevLength === data?.length) return prev;
+        else if (prevLength < data?.length) {
+          NotificationManager.success(
+            `${data.slice(-1)[0]}`,
+            "Successfully checked in"
+          );
+          return data;
+        }
+      });
+    });
+  }, []);
 
   const fetchLoop = async () => {
+    if (stopRef.current === 1) return;
     try {
       let ctx1 = canvas1.current.getContext("2d");
       await ctx1.drawImage(videoRef.current, 0, 0, 640, 480);
-      var dataURL = canvas1.current.toDataURL("image/jpeg", 0.95);
       await canvas1.current.toBlob(
         async (blob) => {
           const res = await recognitionService.recognize(blob, { limit: 1 });
-          console.log(res);
+          const { result } = res;
+          const { box, subjects } = result[0];
+
+          subjects &&
+            socket.emit("newAttendance", {
+              sessionId: sessionId,
+              studentName: subjects[0]?.subject,
+            });
         },
         "image/jpeg",
         0.95
       );
-      if (stopRef.current === 0) {
-        setTimeout(fetchLoop, 3000);
-      }
+      setTimeout(fetchLoop, 3000);
     } catch (err) {
       if (err?.response?.status === 400) setTimeout(fetchLoop, 3000);
       console.error(err);
@@ -70,22 +102,21 @@ const TakeAttendanceFace = () => {
       .getUserMedia({ video: true })
       .then((stream) => {
         videoRef.current.srcObject = stream;
-        receivedMediaStream = stream;
       })
       .catch((err) => console.error("startCam error", err));
-    setIsTakingAttendance(true);
+    setTimeout(fetchLoop, 3000);
   };
 
-  const closeCam = () => {
-    setIsTakingAttendance(false);
-    stopRef = stopRef.current + 1;
+  const closeCam = async () => {
+    stopRef.current = 1;
+    videoRef.current.srcObject.getTracks()[0].stop();
   };
 
   return (
     <>
+      <NotificationContainer />
       <div className="TakeAttendanceFace">
         <div className="videoWrapper">
-          div for face
           <video ref={videoRef} autoPlay muted></video>
           <canvas
             ref={canvas1}
@@ -94,23 +125,19 @@ const TakeAttendanceFace = () => {
             height="480"
             style={{ display: "none" }}
           ></canvas>
+          <div>
+            <button onClick={() => startCam()}>Start taking attendance</button>
+            <button red={stopRef} onClick={() => closeCam()}>
+              stop
+            </button>
+          </div>
         </div>
-        <div className="attendeesListWrapper">div for students<AttendeesList dataSrc={attendees} /></div>
+        <div className="attendeesListWrapper">
+          <AttendeesList dataSrc={attendees} />
+        </div>
       </div>
-      <button onClick={() => startCam()}>Start taking attendance</button>
-      <button red={stopRef} onClick={() => closeCam()}>
-        stop
-      </button>
     </>
   );
 };
 
 export default TakeAttendanceFace;
-
-/* 
-upon loading, connect websocket 
-sessionID, lecturerID, fetchall students that have atttended? , 
-
-Connect to compreface => get attendance as users checkin => pass student name to attendance backend (student name, sessionID)
-
-*/
