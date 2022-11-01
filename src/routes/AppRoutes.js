a9565410f852be6daf35279440fb15e8e9c9c3b4
Tow <chiamtw95@gmail.com>
2022-11-01 08:35:49 +0800

    removed garbage

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -98,7 +98,6 @@ const AppRoutes = () => {
             element={
               <ProtectedRoute restrictedTo="ADMIN">
                 <TakeAttendance />
-                kk{" "}
               </ProtectedRoute>
             }
           />
