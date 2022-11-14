c69e10283c61e66802bab956b37062c7011af63f
Tow <chiamtw95@gmail.com>
2022-11-14 23:38:23 +0800

    added a helper

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -1,2 +1,8 @@
+import { ACESS_TOKEN } from "../constant/token";
+
 export const isTokenExpired = (token) =>
   token?.exp * 1000 <= new Date().valueOf();
+
+export const getToken = () => {
+  return localStorage.getItem(ACESS_TOKEN);
+};
